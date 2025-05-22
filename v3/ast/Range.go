ae9fca19675c491357333917c07845e97aa6d9c4
Derk Norton <derk.norton@gmail.com>
2025-05-22 17:26:44 -0600

    Regenerated the project using an updated syntax.cdsn file.

---
+++ v3/ast/Range.go
@@ -0,0 +1,126 @@
+/*
+................................................................................
+.    Copyright (c) 2009-2025 Crater Dog Technologies.  All Rights Reserved.    .
+................................................................................
+.  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.               .
+.                                                                              .
+.  This code is free software; you can redistribute it and/or modify it under  .
+.  the terms of The MIT License (MIT), as published by the Open Source         .
+.  Initiative. (See https://opensource.org/license/MIT)                        .
+................................................................................
+*/
+
+/*
+┌────────────────────────────────── WARNING ───────────────────────────────────┐
+│                 This class file was automatically generated.                 │
+│                     Any updates to it may be overwritten.                    │
+└──────────────────────────────────────────────────────────────────────────────┘
+*/
+
+package ast
+
+import (
+	uti "github.com/craterdog/go-missing-utilities/v7"
+)
+
+// CLASS INTERFACE
+
+// Access Function
+
+func RangeClass() RangeClassLike {
+	return rangeClass()
+}
+
+// Constructor Methods
+
+func (c *rangeClass_) Range(
+	leftBracket LeftBracketLike,
+	primitive1 PrimitiveLike,
+	delimiter string,
+	primitive2 PrimitiveLike,
+	rightBracket RightBracketLike,
+) RangeLike {
+	if uti.IsUndefined(leftBracket) {
+		panic("The \"leftBracket\" attribute is required by this class.")
+	}
+	if uti.IsUndefined(primitive1) {
+		panic("The \"primitive1\" attribute is required by this class.")
+	}
+	if uti.IsUndefined(delimiter) {
+		panic("The \"delimiter\" attribute is required by this class.")
+	}
+	if uti.IsUndefined(primitive2) {
+		panic("The \"primitive2\" attribute is required by this class.")
+	}
+	if uti.IsUndefined(rightBracket) {
+		panic("The \"rightBracket\" attribute is required by this class.")
+	}
+	var instance = &range_{
+		// Initialize the instance attributes.
+		leftBracket_:  leftBracket,
+		primitive1_:   primitive1,
+		delimiter_:    delimiter,
+		primitive2_:   primitive2,
+		rightBracket_: rightBracket,
+	}
+	return instance
+}
+
+// INSTANCE INTERFACE
+
+// Principal Methods
+
+func (v *range_) GetClass() RangeClassLike {
+	return rangeClass()
+}
+
+// Attribute Methods
+
+func (v *range_) GetLeftBracket() LeftBracketLike {
+	return v.leftBracket_
+}
+
+func (v *range_) GetPrimitive1() PrimitiveLike {
+	return v.primitive1_
+}
+
+func (v *range_) GetDelimiter() string {
+	return v.delimiter_
+}
+
+func (v *range_) GetPrimitive2() PrimitiveLike {
+	return v.primitive2_
+}
+
+func (v *range_) GetRightBracket() RightBracketLike {
+	return v.rightBracket_
+}
+
+// PROTECTED INTERFACE
+
+// Instance Structure
+
+type range_ struct {
+	// Declare the instance attributes.
+	leftBracket_  LeftBracketLike
+	primitive1_   PrimitiveLike
+	delimiter_    string
+	primitive2_   PrimitiveLike
+	rightBracket_ RightBracketLike
+}
+
+// Class Structure
+
+type rangeClass_ struct {
+	// Declare the class constants.
+}
+
+// Class Reference
+
+func rangeClass() *rangeClass_ {
+	return rangeClassReference_
+}
+
+var rangeClassReference_ = &rangeClass_{
+	// Initialize the class constants.
+}
---
+++ v3/ast/RightBracket.go
@@ -0,0 +1,86 @@
+/*
+................................................................................
+.    Copyright (c) 2009-2025 Crater Dog Technologies.  All Rights Reserved.    .
+................................................................................
+.  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.               .
+.                                                                              .
+.  This code is free software; you can redistribute it and/or modify it under  .
+.  the terms of The MIT License (MIT), as published by the Open Source         .
+.  Initiative. (See https://opensource.org/license/MIT)                        .
+................................................................................
+*/
+
+/*
+┌────────────────────────────────── WARNING ───────────────────────────────────┐
+│                 This class file was automatically generated.                 │
+│                     Any updates to it may be overwritten.                    │
+└──────────────────────────────────────────────────────────────────────────────┘
+*/
+
+package ast
+
+import (
+	uti "github.com/craterdog/go-missing-utilities/v7"
+)
+
+// CLASS INTERFACE
+
+// Access Function
+
+func RightBracketClass() RightBracketClassLike {
+	return rightBracketClass()
+}
+
+// Constructor Methods
+
+func (c *rightBracketClass_) RightBracket(
+	any_ any,
+) RightBracketLike {
+	if uti.IsUndefined(any_) {
+		panic("The \"any\" attribute is required by this class.")
+	}
+	var instance = &rightBracket_{
+		// Initialize the instance attributes.
+		any_: any_,
+	}
+	return instance
+}
+
+// INSTANCE INTERFACE
+
+// Principal Methods
+
+func (v *rightBracket_) GetClass() RightBracketClassLike {
+	return rightBracketClass()
+}
+
+// Attribute Methods
+
+func (v *rightBracket_) GetAny() any {
+	return v.any_
+}
+
+// PROTECTED INTERFACE
+
+// Instance Structure
+
+type rightBracket_ struct {
+	// Declare the instance attributes.
+	any_ any
+}
+
+// Class Structure
+
+type rightBracketClass_ struct {
+	// Declare the class constants.
+}
+
+// Class Reference
+
+func rightBracketClass() *rightBracketClass_ {
+	return rightBracketClassReference_
+}
+
+var rightBracketClassReference_ = &rightBracketClass_{
+	// Initialize the class constants.
+}
